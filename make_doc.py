c36d962d148d587899488d8bbca0d3fb0473f5dd
hut <hut@lavabit.com>
2009-12-11 14:07:34 +0100

    make_doc.py to generate documentation

---
+++ make_doc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Generate pydoc documentation and move it to the doc directory.
+THIS WILL DELETE ALL EXISTING HTML FILES IN THAT DIRECTORY, so don't
+store important content there."""
+
+import pydoc, os, sys
+if __name__ == '__main__':
+	docdir = 'doc'
+	os.chdir(sys.path[0])
+	try: os.mkdir(docdir)
+	except: pass
+
+
+	for fname in os.listdir(docdir):
+		if fname.endswith('.html'):
+			os.remove(os.path.join(docdir, fname))
+
+	pydoc.writedocs('.')
+
+	for fname in os.listdir('.'):
+		if fname.endswith('.html'):
+			os.rename(fname, os.path.join(docdir, fname))
+
