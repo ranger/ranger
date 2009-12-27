a666f32b959416d062ac38645dc27f20686d881b
hut <hut@lavabit.com>
2009-12-27 23:41:04 +0100

    test case for keyapi.Wrapper

---
+++ test/tc_keyapi.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__': from __init__ import init; init()
+
+from unittest import TestCase, main
+
+class Test(TestCase):
+	def test_wrapper(self):
+		from ranger.keyapi import Wrapper
+
+		class dummyfm(object):
+			def move(relative):
+				return "I move down by {0}".format(relative)
+
+		class commandarg(object):
+			def __init__(self):
+				self.fm = dummyfm
+
+		arg = commandarg()
+
+		do = Wrapper('fm')
+		command = do.move(relative=4)
+
+		self.assertEqual(command(arg), 'I move down by 4')
+
+if __name__ == '__main__': main()
