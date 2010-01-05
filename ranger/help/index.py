d7039145da050125570eb4064e2ba1fbfd8c2510
hut <hut@lavabit.com>
2010-01-05 15:50:00 +0100

    help: extended + reformatted

--- ranger/help/index.py
+++ ranger/help/index.py
@@ -1,14 +1,24 @@
 """
-You are viewing the help index of ranger.
-Press <number>? to view a certain topic or press j to scroll down
-for a general overview about this program.
+                       Ranger - main help file
+                                                                     k
+    Move around:  Use the cursor keys, or "h" to go left,          h   l
+                  "j" to go down, "k" to go up, "l" to go right.     j
+   Close Ranger:  Use ":q<Enter>" or "Q".
 
-	0?   This index
-	1?   Basic Movement
-	2?   Quickly switching directories
+  Specific help:  Type the help key "?" prepended with a number, as in "4?"
+
+	|0?|	This index
+	|1?|	Basic movement and browsing
+	|2?|	Running Files
+	|3?|	The console
+	|4?|	File operations
+==============================================================================
+
+0.1. General information
+
+Whenever you see an annotation like |3?| you can translate it to:
+"This is explained in more detail in chapter 3. Type "3?" to view it."
 
 
-General overview:
-
-Blablabla
 """
+# vim:tw=78:sw=4:sts=8:ts=8:ft=help
--- ranger/help/movement.py
+++ ranger/help/movement.py
@@ -1,64 +1,100 @@
 """
-1. Basic Movement With Ranger
+1. Basic movement and browsing
 
+1.1. Move around
+1.2. Browser control
+1.3. Searching
+1.4. Cycling
+
+==============================================================================
 1.1. Ranger has similar movement keys as vim:
 
-	k       move up
-	j       move down
-	h       move left (in browser: move one directory up)
-	l       move right (in browser: enter this directory, or run this file)
+	k	move up
+	j	move down
+	h	move left (in browser: move one directory up)
+	l	move right (in browser: enter this directory, or run this file)
 
-	K       move half the screen up
-	J       move half the screen down
-	H       in browser: move back in history
-	L       in browser: move forward in history
+	K	move half the screen up
+	J	move half the screen down
+	H	in browser: move back in history
+	L	in browser: move forward in history
 
-	gg      move to the top
-	G       move to the bottom
+	gg	move to the top
+	G	move to the bottom
+	%	move to the middle
 
 By prefixing a number, you can give more precise commands, eg:
 
-	2J      move 2 pages down
-	5gg     move to the 5th line
-	3h      move 3 characters to the left, or move 3 directories up
+	2J	move 2 pages down
+	5gg	move to the 5th line
+	3h	move 3 characters to the left, or move 3 directories up
+	30%	move to 30% of the screen
 
 Using arrow keys is equivalent of using h/j/k/l in most cases.
 An exception to this is the console, where you can move around with
 arrow keys and pressing letters will insert the letter into the console.
-----------------------------------------
-
-1.2. Special Keys
 
 Special keys like Home, Page Up,.. work as expected.
-The Enter key is equivalent to l except that files will be opened
-in "mode 1" instead of the default mode 0. (5?)
 
-These keys give you more movement control: (^X = Ctrl X)
+These keys work like in vim:
 
-	K, ^D   move half the screen up
-	J, ^U   move half the screen down
+	^D      move half the screen up
+	^U      move half the screen down
 	^B      move up by one screen
 	^F      move down by one screen
-----------------------------------------
 
-1.3. Cycling
+==============================================================================
+1.2. Browser control
 
-You can't only move up and down through the list, but also cycle through
-the items in different logical orders.
+	?	view the help screen
+	^R	clear the cache and reload the view
+	^L	redraw the window
+	:	open the console |3?|
 
-	/       open the "search console" and cycle through matches (3?)
-	TAB     cycle through tagged items
-	cc      cycle by ctime (last modification)
-	cm      cycle by mime type, connecting similar files
-	cs      cycle by size, large items first
+	i	inspect the content of the file
+	E	edit the file
+	s	open a shell, starting in the current directory
 
-	n       move to the next item
-	N       move to the previous item
+Marking files allows you to use operations on multiple files at once.
+If there are any marked files in this directory, "yy" will copy them instead
+of the file you're pointing at.
 
-	See also: sorting (4?)
+	<Space> mark a file
+	v	toggle all marks
+	V	remove all marks
 
-Example:
+By "tagging" files, you can highlight them and mark them to be
+special in whatever context you want.
 
-	csnn    move to the third largest item
-----------------------------------------
+	b	tag/untag a file
+	B	untag a file
+
+==============================================================================
+1.3. Searching
+
+Use "/" to open the search console. Enter a string and press <Enter> to
+search for it in all currently visible files. Pressing "n" will move you
+to the next occurance, "N" to the previous one.
+
+You can search for more than just strings:
+	TAB	search tagged files
+	cc	cycle through all files by their ctime (last modification)
+	cm	cycle by mime type, connecting similar files
+	cs	cycle by size, large items first
+
+==============================================================================
+1.4. Sorting
+
+To sort files, type "o" suffixed with a key that stands for a certain
+sorting mode. By typing any of those keys in upper case, the order will
+be reversed.
+
+	os	sort by size
+	ob, on	sort by basename
+	om	sort by mtime (last modification)
+	ot	sort by mime type
+	or	reverse order
+
+==============================================================================
 """
+# vim:tw=78:sw=4:sts=8:ts=8:ft=help
