8757f653918eb6ccfc5ec6715957e6cd04a2ff3f
hut <hut@lavabit.com>
2010-01-10 22:26:33 +0100

    help: added copying

--- ranger/help/index.py
+++ ranger/help/index.py
@@ -34,5 +34,23 @@ Annotations like |1?| indicate that the topic is explained in more
 detail in chapter 1. You can type 1? to view it.
 
 ==============================================================================
+
+0.2 Copying
+
+Ranger is free software licensed under the terms of the OpenBSD license.
+
+Copyright (c) 2009, 2010 hut <hut@lavabit.com>
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 """
 # vim:tw=78:sw=4:sts=8:ts=8:ft=help
