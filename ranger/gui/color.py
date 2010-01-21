c8f40bb1693ea8fbb4923c547e7c0a5f89fa0282
hut <hut@lavabit.com>
2010-01-21 04:36:53 +0100

    color: added a remove_attr function

--- ranger/gui/color.py
+++ ranger/gui/color.py
@@ -60,3 +60,9 @@ underline  = curses.A_UNDERLINE
 invisible  = curses.A_INVIS
 
 default_colors = (default, default, normal)
+
+def remove_attr(integer, attr):
+	"""Remove an attribute from an integer"""
+	if integer & attribute:
+		return integer ^ attribute
+	return integer
