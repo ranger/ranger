9c27c9626a44d19276a2e03ca907a4a159cd464f
hut <hut@lavabit.com>
2010-02-15 22:02:32 +0100

    data/generate.py: allow spaces as seperators in mimetypes

--- ranger/data/generate.py
+++ ranger/data/generate.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 	table = {}
 
 	for line in open(len(sys.argv) > 1 and sys.argv[1] or "mime.types"):
-		if len(line) > 3 and line[0] != '#' and '\t' in line:
+		if len(line) > 3 and line[0] != '#' and ('\t' in line or ' ' in line):
 			name, *extensions = line.split()
 			for ext in extensions:
 				table[ext] = name
