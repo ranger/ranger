811b7c282fdb3032659626190b936c3365fa4dc1
hut <hut@lavabit.com>
2010-02-28 13:53:16 +0100

    Changed license to the GNU General Public License

--- ranger/__init__.py
+++ ranger/__init__.py
@@ -1,16 +1,17 @@
-# Copyright (c) 2009, 2010 hut <hut@lavabit.com>
+# Copyright (C) 2009, 2010  Roman Zimbelmann <romanz@lavabit.com>
 #
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Ranger - file browser for the unix terminal"""
 
@@ -20,13 +21,16 @@ import sys
 # for easier access
 from ranger.ext.debug import log, trace
 
-__copyright__ = 'none'
-__license__ = 'ISC'
+__copyright__ = """
+Copyright (C) 2009, 2010  Roman Zimbelmann <romanz@lavabit.com>
+"""
+
+__license__ = 'GPL3'
 __version__ = '1.0.3'
-__credits__ = 'hut'
-__author__ = 'hut'
-__maintainer__ = 'hut'
-__email__ = 'hut@lavabit.com'
+__credits__ = 'Roman Zimbelmann'
+__author__ = 'Roman Zimbelmann'
+__maintainer__ = 'Roman Zimbelmann'
+__email__ = 'romanz@lavabit.com'
 
 debug = False
 
--- ranger/gui/widgets/titlebar.py
+++ ranger/gui/widgets/titlebar.py
@@ -1,16 +1,17 @@
-# Copyright (c) 2009, 2010 hut <hut@lavabit.com>
+# Copyright (C) 2009, 2010  Roman Zimbelmann <romanz@lavabit.com>
 #
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 The titlebar is the widget at the top, giving you broad orientation.
