62cd83bace8e77cd1ff7028da6cf65d0d1defa27
hut <hut@lavabit.com>
2010-03-12 02:00:53 +0100

    updated pydoc

--- ranger/core/fm.py
+++ ranger/core/fm.py
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+The File Manager, putting the pieces together
+"""
+
 from time import time
 from collections import deque
 
