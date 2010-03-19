0072c11662c916dfe8a34de7c9a680d939ac0b79
hut <hut@lavabit.com>
2010-03-19 19:44:06 +0100

    gui.widgets.pager: increase the width of the pager by 1

--- ranger/gui/widgets/pager.py
+++ ranger/gui/widgets/pager.py
@@ -234,7 +234,7 @@ class Pager(Widget):
 		while True:
 			try:
 				line = self._get_line(i).expandtabs(4)
-				line = line[startx:self.wid - 1 + startx].rstrip()
+				line = line[startx:self.wid + startx].rstrip()
 				yield line
 			except IndexError:
 				raise StopIteration
