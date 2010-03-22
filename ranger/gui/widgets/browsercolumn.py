ranger/ranger

commit fe14923beb97ec60c65e5eb6f5cb9ee0bc303e15

widgets.browsercolumn: don't load preview when its turned off

ranger/gui/widgets/browsercolumn.py
@@ -124,7 +124,8 @@ class BrowserColumn(Pager):
 			self.need_redraw = True
 			self.old_dir = self.target
 
-		if self.target and self.target.is_directory:
+		if self.target and self.target.is_directory \
+				and self.settings.preview_directories:
 			if self.target.pointed_obj != self.old_cf:
 				self.need_redraw = True
 				self.old_cf = self.target.pointed_obj

commit b712bbcf472326b8d748421bf2448acbfe0f92bf

fixes small bug introduced in fe14923

ranger/gui/widgets/browsercolumn.py
@@ -125,7 +125,7 @@ class BrowserColumn(Pager):
 			self.old_dir = self.target
 
 		if self.target and self.target.is_directory \
-				and self.settings.preview_directories:
+				and (self.level <= 0 or self.settings.preview_directories):
 			if self.target.pointed_obj != self.old_cf:
 				self.need_redraw = True
 				self.old_cf = self.target.pointed_obj