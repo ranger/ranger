356726c41d5f7a95fdf60ed867f05d2d10bff70a
hut <hut@lavabit.com>
2010-06-22 01:44:48 +0200

    data/scope.sh: improved, commented

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-# This script is responsible to generate the previews for ranger.
+
+# This script is called whenever you preview a file.
+# Its output is used as the preview.  ANSI color codes are supported.
+
+# Meaning of exit codes:
+# code | meaning    | action of ranger
+# -----+------------+-------------------------------------------
+# 0    | success    | display stdout as preview
+# 1    | no preview | display no preview at all
+
+
 mimetype=$(file --mime-type -Lb "$1")
-basetype=$(echo "$mimetype" | grep -o '^[^/]\+')
 extension=$(echo "$1" | grep '\.' | grep -o '[^.]\+$')
 
-case "$basetype" in
-	text)
-		highlight --ansi "$1" || cat "$1"
+case "$mimetype" in
+	text/*)
+		highlight --ansi "$1" || cat "$1" || exit 1
 		exit 0;;
-	image)
+	image/*)
 		img2txt "$1" || exit 1
 		exit 0;;
 esac
 
 case "$extension" in
 	zip|gz)
-		atool -l "$1"
+		atool -l "$1" || exit 1
 		exit 0;;
 esac
 
