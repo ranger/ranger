bf14a7e72e7c03673949388365744f26e227ec83
hut <hut@lavabit.com>
2010-06-22 11:27:21 +0200

    data/scope.sh: added lynx

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -13,18 +13,26 @@
 mimetype=$(file --mime-type -Lb "$1")
 extension=$(echo "$1" | grep '\.' | grep -o '[^.]\+$')
 
-case "$mimetype" in
-	text/*)
-		highlight --ansi "$1" || cat "$1" || exit 1
+case "$extension" in
+	# Archive extensions:
+	tar|gz|tgz|bz|tbz|bz2|tbz2|Z|tZ|lzo|tzo|lz|tlz|xz|txz|7z|t7z|\
+	zip|jar|war|rar|lha|lzh|alz|ace|a|arj|arc|rpm|cab|lzma|rz|cpio)
+		atool -l "$1" || exit 1
 		exit 0;;
-	image/*)
-		img2txt "$1" || exit 1
+	# HTML Pages:
+	htm|html|xhtml)
+		lynx -dump "$1" || elinks -dump "$1" || exit 1
 		exit 0;;
 esac
 
-case "$extension" in
-	zip|gz)
-		atool -l "$1" || exit 1
+case "$mimetype" in
+	# Syntax highlight for text files:
+	text/* | */xml)
+		highlight --ansi "$1" || cat "$1" || exit 1
+		exit 0;;
+	# Ascii-previews of images:
+	image/*)
+		img2txt "$1" || exit 1
 		exit 0;;
 esac
 
