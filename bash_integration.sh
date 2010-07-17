2557605b1046cfa8b925ba3cf6d927ac5a31a24b
hut <hut@lavabit.com>
2010-07-17 14:01:47 +0200

    slim: changed concept of bash_integration.sh
    source it in your bashrc rather than on each start of ranger

--- bash_integration.sh
+++ bash_integration.sh
@@ -1,27 +1,35 @@
-# You should source this script, passing the command to start ranger
-# as the arguments.
+# You should source this script in your bashrc, passing the command to start
+# ranger as the arguments.  Then, start ranger with the command "bash_ranger"
+# or by typing the shortcut ^O
 #
-# Example command:
-# source bash_integration.sh python3 /path/to/ranger.py
+# Example bashrc entry:
+# source /path/to/bash_integration.sh python3 /path/to/ranger.py
+# alias ranger=bash_ranger
 
-CACHEDIR=$HOME/.cache/ranger
-[ -n "$XDG_CACHE_HOME" ] && CACHEDIR=$XDG_CACHE_HOME/ranger
+RANGER_EXEC="$@"
 
-PWD_BEFORE="$(pwd)"
-$@ "$PWD_BEFORE"
-RETURN_VALUE=$?
-PWD_AFTER="$(cat "$CACHEDIR"/last_dir)"
+function bash_ranger {
+	CACHEDIR=$HOME/.cache/ranger
+	[ -n "$XDG_CACHE_HOME" ] && CACHEDIR=$XDG_CACHE_HOME/ranger
 
-if [ "$PWD_BEFORE" != "$PWD_AFTER" -a "$RETURN_VALUE" == 0 ]; then
-	cd "$PWD_AFTER"
-fi
+	PWD_BEFORE="$(pwd)"
+	$RANGER_EXEC "$PWD_BEFORE"
+	RETURN_VALUE=$?
+	PWD_AFTER="$(cat "$CACHEDIR"/last_dir)"
 
-export RANGER_POINTER="$(cat "$CACHEDIR"/last_pointer)"
-export RANGER_SCROLL_START="$(cat "$CACHEDIR"/last_scroll_start)"
+	if [ "$PWD_BEFORE" != "$PWD_AFTER" -a "$RETURN_VALUE" == 0 ]; then
+		cd "$PWD_AFTER"
+	fi
 
-d="$(cat "$CACHEDIR"/last_dir)"
-f="$RANGER_POINTER"
+	export RANGER_POINTER="$(cat "$CACHEDIR"/last_pointer)"
+	export RANGER_SCROLL_START="$(cat "$CACHEDIR"/last_scroll_start)"
+
+	d="$(cat "$CACHEDIR"/last_dir)"
+	f="$RANGER_POINTER"
+}
 
 function sel {
 	xargs -d "\n" "$@" < "$CACHEDIR"/last_selection
 }
+
+bind -x '"\C-o": bash_ranger'
