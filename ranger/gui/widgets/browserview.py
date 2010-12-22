cc69d0d8867b02ea8c9fa96a58e46a2ce31b24ce
hut <hut@lavabit.com>
2010-12-22 01:59:00 +0100

    widgets.browserview: Cleaner solution for commit 3fd5f5caf6

--- ranger/gui/widgets/browserview.py
+++ ranger/gui/widgets/browserview.py
@@ -289,5 +289,13 @@ class BrowserView(Widget, DisplayableContainer):
 
 	def poke(self):
 		DisplayableContainer.poke(self)
+
+		# Show the preview column when it has a preview but has
+		# been hidden (e.g. because of padding_right = False)
+		if not self.pager.visible and not self.columns[-1].visible and \
+		self.columns[-1].target and self.columns[-1].target.is_directory \
+		or self.columns[-1].has_preview():
+			self.columns[-1].visible = True
+
 		if self.preview and self.is_collapsed != self._collapse():
 			self.resize(self.y, self.x, self.hei, self.wid)
