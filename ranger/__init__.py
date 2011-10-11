531d8b2def5fcf43d70819c86fc612028ca98f4d
hut <hut@lavabit.com>
2011-10-11 22:37:39 +0200

    FOR THE HURD

--- ranger/__init__.py
+++ ranger/__init__.py
@@ -25,7 +25,7 @@ import os
 
 # Information
 __license__ = 'GPL3'
-__version__ = '1.5.0'
+__version__ = '1.5.0 (stable)'
 __author__ = __maintainer__ = 'Roman Zimbelmann'
 __email__ = 'romanz@lavabit.com'
 
