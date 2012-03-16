4f5a4bed219324168a2f2e828cbf2e07b37b4aa8
hut <hut@lavabit.com>
2012-03-16 22:55:20 +0100

    container.tags: typo

--- ranger/container/tags.py
+++ ranger/container/tags.py
@@ -37,7 +37,7 @@ class Tags(object):
 		if 'tag' in others:
 			tag = others['tag']
 		else:
-			tag = self.defautag
+			tag = self.default_tag
 		self.sync()
 		for item in items:
 			self.tags[item] = tag
