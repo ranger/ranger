146f51d6901bceac5b6c6447d967867948614268
hut <hut@lavabit.com>
2013-03-01 14:08:21 +0100

    ranger.fsobject: added BAD_INFO constant

--- ranger/fsobject.py
+++ ranger/fsobject.py
@@ -1,5 +1,5 @@
 # THIS WHOLE FILE IS OBSOLETE AND EXISTS FOR BACKWARDS COMPATIBILITIY
 
-from ranger.container.fsobject import FileSystemObject
+from ranger.container.fsobject import FileSystemObject, BAD_INFO
 from ranger.container.file import File
 from ranger.container.directory import Directory
