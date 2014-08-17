5210a0d5b022725ff5e0165ec5bd7de5f84ecc8d
rukai <rubickent@gmail.com>
2014-08-17 20:10:36 +1000

    Added renameConsole a command which automatically creates a console for the rename command and places the cursor before the file extension.

--- ranger/config/commands.py
+++ ranger/config/commands.py
@@ -685,6 +685,18 @@ class rename(Command):
     def tab(self):
         return self._tab_directory_content()
 
+class renameConsole(Command):
+    """:renameCommand
+
+    Creates an open_console for the rename command, automatically placing the cursor before the file extension.
+    """
+
+    def execute(self):
+        if "." in self.fm.thisfile.basename:
+            offset = 6 + len(self.fm.thisfile.basename) - self.fm.thisfile.basename[::-1].index('.')
+            self.fm.open_console('rename ' + self.fm.thisfile.basename, position=offset)
+        else:
+            self.fm.open_console('rename ' + self.fm.thisfile.basename)
 
 class chmod(Command):
     """:chmod <octal number>
