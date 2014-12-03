7417def6db0b5e9113335bd35e75ce1135e59253
hut <hut@lepus.uberspace.de>
2014-12-03 18:28:00 +0100

    core.actions: map linemode "normal" to "filemode"

--- ranger/core/actions.py
+++ ranger/core/actions.py
@@ -155,12 +155,16 @@ class Actions(FileManagerAware, EnvironmentAware, SettingsAware):
         """
         Change what is displayed as a filename.
 
-        "mode" may be: "filename", "permissions", "title"
-        "directory" specifies the directory. None means the current directory
-        "depth" specifies the recursion depth
+        - "mode" may be: "filename", "permissions", "title", the mode "normal"
+          is mapped to "filename".
+        - "directory" specifies the directory. None means the current directory
+        - "depth" specifies the recursion depth
         """
 
-        assert mode in ("filename", "permissions", "title")
+        assert mode in ("normal", "filename", "permissions", "title")
+
+        if mode == "normal":
+            mode = "filename"
 
         if directory is None:
             directory = self.fm.thisdir
