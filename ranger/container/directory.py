616e1fb8357dab1c588a5507ba183206755e5a74
hut <hut@lepus.uberspace.de>
2014-12-10 03:45:01 +0100

    container.directory: Fixed :scout -tf, fixes #195
    This was a regression from f9b4e8c0a31d38fb48ee0e61e17a9534f00fdd82

--- ranger/container/directory.py
+++ ranger/container/directory.py
@@ -206,6 +206,9 @@ class Directory(FileSystemObject, Accumulator, Loadable):
         filters = [(lambda file: not hidden_filter.search(file.basename)) if hidden_filter else None,
                    (lambda file: self.filter.search(file.basename)) if self.filter else None,
                    self.inode_type_filter]
+
+        if self.temporary_filter:
+            filters.append(lambda file: self.temporary_filter.search(file.basename))
         self.files = [f for f in self.files_all if accept_file(f, filters)]
         self.move_to_obj(self.pointed_obj)
 
