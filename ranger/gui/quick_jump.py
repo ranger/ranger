f0c33daa379123d8df40f0ca788e308af171fa36
Klinkenstecker <klinkenstecker@gmx.de>
2014-12-25 22:01:19 +0100

    sometimes flags where removed from scout in the KEEP_OPEN case

--- ranger/gui/quick_jump.py
+++ ranger/gui/quick_jump.py
@@ -38,7 +38,8 @@ class QuickJump:
     def activate(self, scout):
         self.scout = scout
         settings = self.fm.settings
-        newState = scout.QUICK_JUMP in scout.flags or scout.QUICK_PAUSED in scout.flags
+        newState = scout.QUICK_JUMP in scout.flags or \
+                   scout.QUICK_PAUSED in scout.flags
         if self.activated != newState:
             self.activated = newState
             self.fm.ui.browser.main_column.request_redraw()
