8151f65ce46156f2c70867f9c3be5f73426d316f
hut <hut@lepus.uberspace.de>
2015-03-18 19:31:18 +0100

    renamed `ranger.api.custom_linemode` back to `...register_linemode`

--- ranger/api/__init__.py
+++ ranger/api/__init__.py
@@ -29,7 +29,7 @@ def hook_ready(fm):
 
 from ranger.core.linemode import LinemodeBase
 
-def custom_linemode(linemode_class):
+def register_linemode(linemode_class):
     from ranger.container.fsobject import FileSystemObject
     FileSystemObject.linemode_dict[linemode_class.name] = linemode_class()
     return linemode_class
