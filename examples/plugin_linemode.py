1bcb5c9c4b6396f935cfd46e6ad38f7a295fab43
hut <hut@lepus.uberspace.de>
2015-04-19 02:06:01 +0200

    added example plugin that adds a linemode

---
+++ examples/plugin_linemode.py
@@ -0,0 +1,16 @@
+# Compatible since ranger 1.7.0
+#
+# This sample plugin adds a new linemode displaying the filename in rot13.
+# Load this plugin by copying it to ~/.config/ranger/plugins/ and activate
+# the linemode by typing ":linemode rot13" in ranger.  Type Mf to restore
+# the default linemode.
+
+import codecs
+import ranger.api
+from ranger.core.linemode import LinemodeBase
+
+@ranger.api.register_linemode
+class MyLinemode(LinemodeBase):
+    name = "rot13"
+    def filetitle(self, file, metadata):
+        return codecs.encode(file.basename, "rot_13")
