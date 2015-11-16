de0830a5fa570552aee93a4d0fe31f223bfb2640
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-11-16 17:32:13 +0100

    ext.version_check: Remove commands_full.py from the check
    This file is no loaded so there is no need to check it. If the user
    renames it to commands.py, the check will be perfomed.

--- ranger/ext/version_check.py
+++ ranger/ext/version_check.py
@@ -9,7 +9,6 @@ import re
 _compatibility = {
     'scope.sh': (1,7,9999),
     'commands.py': (1,7,9999),
-    'commands_full.py': (1,7,9999),
     'rifle.conf': (1,7,9999),
     'rc.conf': (1,7,9999),
 }
