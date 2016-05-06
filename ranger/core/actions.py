523689c114f0688e8efb3831e13e5214a2a858f3
hut <hut@lepus.uberspace.de>
2016-05-06 23:21:05 +0200

    core.actions: fix calculation of program indices
    The solutoin of @NoSuck used to break when you used "number 9999" in a
    declaration of some program.

--- ranger/core/actions.py
+++ ranger/core/actions.py
@@ -824,10 +824,11 @@ class Actions(FileManagerAware, SettingsAware):
             return
         programs = [program for program in self.rifle.list_commands([target.path],
                 None)]
-        num_digits = len(str(len(programs)))
-        program_info = ['%s | %s' % (str(program[0]).rjust(num_digits),
-                program[1]) for program in programs]
-        self.ui.browser.draw_info = program_info
+        if programs:
+            num_digits = max((len(str(program[0])) for program in programs))
+            program_info = ['%s | %s' % (str(program[0]).rjust(num_digits),
+                    program[1]) for program in programs]
+            self.ui.browser.draw_info = program_info
 
     def hide_console_info(self):
         self.ui.browser.draw_info = False
