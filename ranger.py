ac51d814046c1210611b0a7e0d753ac8bfae93d9
stepshal <nessento@openmailbox.org>
2016-06-14 22:33:44 +0700

    ranger.py: place imports at the top of the module

--- ranger.py
+++ ranger.py
@@ -2,6 +2,10 @@
 # This file is part of ranger, the console file manager.  (coding: utf-8)
 # License: GNU GPL version 3, see the file "AUTHORS" for details.
 
+import sys
+from os.path import exists, abspath
+import ranger
+
 # =====================
 # This embedded bash script can be executed by sourcing this file.
 # It will cd to ranger's last location after you exit it.
@@ -22,9 +26,6 @@ rm -f -- "$tempfile"
 return $returnvalue
 """ and None
 
-import sys
-from os.path import exists, abspath
-
 # Need to find out whether or not the flag --clean was used ASAP,
 # because --clean is supposed to disable bytecode compilation
 argv = sys.argv[1:sys.argv.index('--')] if '--' in sys.argv else sys.argv[1:]
@@ -35,5 +36,4 @@ if __file__[:4] == '/usr' and exists('ranger') and abspath('.') in sys.path:
     sys.path.remove(abspath('.'))
 
 # Start ranger
-import ranger
 sys.exit(ranger.main())
