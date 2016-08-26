d2f18170cae321cde246c4f96e4cf56a49a90689
Florian Sattler <sflorian92@uni-koblenz.de>
2016-08-26 22:48:34 +0200

    Added preview option for odt, ods, odp and sxw files using odt2txt

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -80,6 +80,9 @@ case "$extension" in
     # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
+    # ODT Files
+    odt|ods|odp|sxw)
+        try odt2txt "$path" && { dump | trim; exit 5; } || exit 1;;
     # HTML Pages:
     htm|html|xhtml)
         try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
