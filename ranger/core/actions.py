74b6cc71471f419a4fb6b90c0906d11c085e0ba6
Profpatsch <mail@profpatsch.de>
2016-09-28 22:35:49 +0200

    PAGER: split on " "
    $PAGER could be something like `"less -R"`, and `-R` should be given as
    argument, not as executable.

--- ranger/core/actions.py
+++ ranger/core/actions.py
@@ -4,6 +4,7 @@
 import codecs
 import os
 import re
+import shlex
 import shutil
 import string
 import tempfile
@@ -1186,8 +1187,7 @@ class Actions(FileManagerAware, SettingsAware):
             write("\n")
 
         temporary_file.flush()
-        pager = os.environ.get('PAGER', ranger.DEFAULT_PAGER)
-        self.run([pager, temporary_file.name])
+        self._call_pager(temporary_file)
 
     def dump_commands(self):
         temporary_file = tempfile.NamedTemporaryFile()
@@ -1212,8 +1212,7 @@ class Actions(FileManagerAware, SettingsAware):
                 write("    :%s\n" % cmd.get_name())
 
         temporary_file.flush()
-        pager = os.environ.get('PAGER', ranger.DEFAULT_PAGER)
-        self.run([pager, temporary_file.name])
+        self._call_pager(temporary_file)
 
     def dump_settings(self):
         temporary_file = tempfile.NamedTemporaryFile()
@@ -1225,8 +1224,13 @@ class Actions(FileManagerAware, SettingsAware):
             write("%30s = %s\n" % (setting, getattr(self.settings, setting)))
 
         temporary_file.flush()
+        self._call_pager(temporary_file)
+
+    def _call_pager(self, file_):
         pager = os.environ.get('PAGER', ranger.DEFAULT_PAGER)
-        self.run([pager, temporary_file.name])
+        # PAGER might contain arguments, like "less -R"
+        pager = shlex.split(pager)
+        self.run(pager + [file_.name])
 
     # --------------------------
     # -- File System Operations
