bfaeb3fbadb5152a98b95199403fb1563d730453
hut <hut@lepus.uberspace.de>
2016-12-24 17:40:40 +0100

    changed version label "stable" back to "master"

--- ranger/__init__.py
+++ ranger/__init__.py
@@ -27,7 +27,7 @@ MACRO_DELIMITER = '%'
 DEFAULT_PAGER = 'less'
 CACHEDIR = os.path.expanduser("~/.cache/ranger")
 USAGE = '%prog [options] [path]'
-VERSION = 'ranger-stable %s\n\nPython %s' % (__version__, sys.version)
+VERSION = 'ranger-master %s\n\nPython %s' % (__version__, sys.version)
 
 
 # If the environment variable XDG_CONFIG_HOME is non-empty, CONFDIR is ignored
