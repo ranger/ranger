9572d80597aac9575615b488b894fd4d81490bd4
nfnty <git@nfnty.se>
2017-02-14 23:39:33 +0100

    linting: pylint: `sys.stdout.buffer.write`: Fix `no-member`

--- ranger/ext/img_display.py
+++ ranger/ext/img_display.py
@@ -214,7 +214,7 @@ class ITerm2ImageDisplayer(ImageDisplayer, FileManagerAware):
         if sys.version_info[0] < 3:
             sys.stdout.write(tparm)
         else:
-            sys.stdout.buffer.write(tparm)
+            sys.stdout.buffer.write(tparm)  # pylint: disable=no-member
         sys.stdout.write(self._generate_iterm2_input(path, width, height))
         curses.putp(curses.tigetstr("rc"))
         sys.stdout.flush()
