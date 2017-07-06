27c21c33364860cd3b7e3b3887dc6672a0d2d65d
hut <hut@hut.pm>
2017-07-06 22:00:14 +0200

    added barebone vfs module

---
+++ ranger/vfs/local.py
@@ -0,0 +1,6 @@
+# This file is part of ranger, the console file manager.
+# License: GNU GPL version 3, see the file "AUTHORS" for details.
+
+"""
+An abstraction layer over the local file system
+"""
