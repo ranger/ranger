f51beb183c9d83a248a04cca9f2c85b0c07e50a7
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2017-12-17 21:31:50 +0100

    Pylint & Flake8: Add a local rule exception
    Somehow "too-many-locals" was not failing before, probably because
    Pylint got updated.
    
    The list of Pylint exceptions made the line too long for Flake8 so
    this check was disabled too for this line.

--- ranger/ext/rifle.py
+++ ranger/ext/rifle.py
@@ -296,7 +296,7 @@ class Rifle(object):  # pylint: disable=too-many-instance-attributes
                     count = self._skip
                 yield (count, cmd, self._app_label, self._app_flags)
 
-    def execute(self, files,  # pylint: disable=too-many-branches,too-many-statements
+    def execute(self, files,  # noqa: E501 pylint: disable=too-many-branches,too-many-statements,too-many-locals
                 number=0, label=None, flags="", mimetype=None):
         """Executes the given list of files.
 
