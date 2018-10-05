d5d668a62faeea07a7476a14fef0f63ea84a83e8
LeLobster <nhoise@live.nl>
2018-10-05 03:14:45 +0200

    Check the env variable TERM before setting w3m offset

--- ranger/ext/img_display.py
+++ ranger/ext/img_display.py
@@ -219,8 +219,13 @@ class W3MImageDisplayer(ImageDisplayer, FileManagerAware):
             width = (width * max_height_pixels) // height
             height = max_height_pixels
 
-        start_x = int(((start_x - 0.2) * fontw) + self.fm.settings.st_border_offset)
-        start_y = (start_y * fonth) + self.fm.settings.st_border_offset
+        if os.getenv('TERM') in ['st-256color', 'st']:
+            start_x = int(((start_x - 0.2) * fontw) + self.fm.settings.st_border_offset)
+            start_y = (start_y * fonth) + self.fm.settings.st_border_offset
+        else:
+             start_x = int((start_x - 0.2) * fontw)
+             start_y = start_y * fonth
+                         
 
         return "0;1;{x};{y};{w};{h};;;;;{filename}\n4;\n3;\n".format(
             x=start_x,
