86c16ab4b05ccb99afba247a74ab6875e1407075
Gustav Larsson <gustav.e.larsson@gmail.com>
2018-11-04 00:06:32 +0200

    Exit if STDIN is closed #1367
    Instead of entering a busy-loop if STDIN is closed, terminate ranger
    
    Fixes #1367
    Fixes #87
    Fixes #525
    Fixed #1534

--- ranger/gui/ui.py
+++ ranger/gui/ui.py
@@ -277,6 +277,10 @@ class UI(  # pylint: disable=too-many-instance-attributes,too-many-public-method
                 else:
                     if not self.fm.input_is_blocked():
                         self.handle_key(key)
+            elif key == -1 and not os.isatty(sys.stdin.fileno()):
+                # STDIN has been closed
+                self.fm.exit()
+
 
     def setup(self):
         """Build up the UI by initializing widgets."""
