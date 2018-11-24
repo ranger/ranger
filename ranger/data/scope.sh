bae44ae3c7440c00acf65ba4c26a0fd2a0f78c3a
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-11-24 15:37:23 +0100

    scope.sh: Format JSON with either jq or json.tool
    Closes #1383.

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -83,6 +83,11 @@ handle_extension() {
             lynx -dump -- "${FILE_PATH}" && exit 5
             elinks -dump "${FILE_PATH}" && exit 5
             ;; # Continue with next handler on failure
+        # JSON
+        json)
+            jq --color-output . "${FILE_PATH}" && exit 5
+            python -m json.tool -- "${FILE_PATH}" && exit 5
+            ;;
     esac
 }
 
