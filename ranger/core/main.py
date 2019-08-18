9511bd6e6b0d8a3e8ce04b723cf4903b81db1b61
Toon Nolten <toon.nolten@student.kuleuven.be>
2019-08-18 15:05:08 +0200

    Drop truth values; rename warning setting

--- ranger/core/main.py
+++ ranger/core/main.py
@@ -183,10 +183,10 @@ def main(
         if int(os.environ[level]) > 1:
             warning = 'Warning:'
             nested_warning = "You're in a nested ranger instance!"
-            nrw = fm.settings.nested_ranger_warning.lower()
-            if nrw in ['true', 'yes', 'enabled']:
+            warn_when_nested = fm.settings.nested_ranger_warning.lower()
+            if warn_when_nested == 'true':
                 fm.notify(' '.join((warning, nested_warning)), bad=False)
-            elif nrw == 'bad':
+            elif warn_when_nested == 'error':
                 fm.notify(' '.join((warning.upper(), nested_warning + '!!')),
                           bad=True)
 
