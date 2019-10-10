e2ad29e1dc7fc5c723d95d9710db3b41b8fa6ea1
Victor Lopez <bigbigtor@users.noreply.github.com>
2019-10-10 18:13:16 +0000

    add date_format and time_format default values, they still need a way to force them to get loaded before date_and_time_format

--- ranger/container/settings.py
+++ ranger/container/settings.py
@@ -202,6 +202,10 @@ class Settings(SignalDispatcher, FileManagerAware):
                 value = self.fm.confpath('scope.sh')
                 if not os.path.exists(value):
                     value = self.fm.relpath('data/scope.sh')
+        elif name == 'date_format':
+            value = '%%x'
+        elif name == 'time_format':
+            value = '%%X'
         elif name == 'date_and_time_format':
             value = '{0} {1}'.format(self._settings.get('date_format'),
                                      self._settings.get('time_format'))
