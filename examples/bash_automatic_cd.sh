fa670720e20212c70b19b1b7949ae0c36763c819
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-10-28 12:45:25 +0200

    Removed replaced $(<...) with cat

--- examples/bash_automatic_cd.sh
+++ examples/bash_automatic_cd.sh
@@ -11,7 +11,7 @@ ranger-cd() {
     local temp_file chosen_dir
     temp_file="$(mktemp -t "${0}.XXXXXXXXXX")"
     ranger --choosedir="$temp_file" -- "${@:-$PWD}"
-    if chosen_dir="$(<"$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
+    if chosen_dir="$(cat "$temp_file")" && [ -n "$chosen_dir" ] && [ "$chosen_dir" != "$PWD" ]; then
         cd -- "$chosen_dir"
     fi
     rm -f -- "$temp_file"
