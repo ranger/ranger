77154c4e07d2ccb2288361e303b2484523947189
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-06 22:30:20 +0200

    Added a notice about sourcing examples/shell_subshell_notice.sh as well

--- examples/shell_subshell_notice.sh
+++ examples/shell_subshell_notice.sh
@@ -2,6 +2,7 @@
 #
 # Change the prompt when you open a shell from inside ranger
 #
-# Add this line to your shell startup file (.bashrc, .zshrc etc) for it to work.
+# Source this file from your shell startup file (.bashrc, .zshrc etc) for it to
+# work.
 
 [ -n "$RANGER_LEVEL" ] && PS1="$PS1"'(in ranger) '
