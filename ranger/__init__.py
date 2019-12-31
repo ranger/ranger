3965e3eeafac8ce44670cfdc25ccc7b5997a3366
toonn <toonn@toonn.io>
2019-12-31 16:55:10 +0100

    Flip the __release__ flag

--- ranger/__init__.py
+++ ranger/__init__.py
@@ -36,7 +36,7 @@ def version_helper():
 # Information
 __license__ = 'GPL3'
 __version__ = '1.9.3'
-__release__ = True
+__release__ = False
 __author__ = __maintainer__ = 'Roman Zimbelmann'
 __email__ = 'hut@hut.pm'
 
