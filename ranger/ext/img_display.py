9a1f732e3f79b199854df37cfdb71b119cdd1d37
mayoumin <mayoumin@foxmail.com>
2020-04-10 04:10:22 +0800

    modified file: ranger/ext/img_display.py    - Fix the problem that the image can't preview correctly

--- ranger/ext/img_display.py
+++ ranger/ext/img_display.py
@@ -189,7 +189,6 @@ class W3MImageDisplayer(ImageDisplayer, FileManagerAware):
         self.process.stdin.write(input_gen)
         self.process.stdin.flush()
         self.process.stdout.readline()
-        self.quit()
         self.is_initialized = False
 
     def clear(self, start_x, start_y, width, height):
@@ -216,6 +215,7 @@ class W3MImageDisplayer(ImageDisplayer, FileManagerAware):
             raise
         self.process.stdin.flush()
         self.process.stdout.readline()
+        self.quit()
 
     def _generate_w3m_input(self, path, start_x, start_y, max_width, max_height):
         """Prepare the input string for w3mimgpreview
