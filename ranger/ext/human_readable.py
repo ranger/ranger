c64bf163497e6f33956b747130a0b3e0547a4cde
Mojmír Křížek <mojmirkrizek@gmail.com>
2020-04-13 23:54:57 +0200

    Factor out numerical part of human_readable()
    See PR #1891 for research details.

--- ranger/ext/human_readable.py
+++ ranger/ext/human_readable.py
@@ -19,6 +19,8 @@ def human_readable(byte, separator=' '):  # pylint: disable=too-many-return-stat
     '1023 M'
     """
 
+    units = ['B', 'K', 'M', 'G', 'T', 'P']
+
     # handle automatically_count_files false
     if byte is None:
         return ''
@@ -26,13 +28,20 @@ def human_readable(byte, separator=' '):  # pylint: disable=too-many-return-stat
     if SettingsAware.settings.size_in_bytes:
         return format(byte, 'n')  # 'n' = locale-aware separator.
 
+    [formatted_number, unit_index] = _preformat_bytes(byte)
+    if unit_index is None:
+        return formatted_number
+    return '{0:s}{1:s}{2:s}'.format(formatted_number, separator, units[unit_index])
+
+
+def _preformat_bytes(byte):  # pylint: disable=too-many-return-statements
     # I know this can be written much shorter, but this long version
     # performs much better than what I had before.  If you attempt to
     # shorten this code, take performance into consideration.
     #
     # Revisit of 2020: In order to implement file copy speed with
     # correct units, this had to be refactored. While we are at it,
-    # let's use format functions. It now has about 2 times worse
+    # let's use format functions. It now has about 3 times worse
     # performance than the original solution using %-formatting. The
     # justification here is this function doesn't account for much
     # execution time in the overall rendering time of Ranger. The
@@ -42,30 +51,30 @@ def human_readable(byte, separator=' '):  # pylint: disable=too-many-return-stat
     # performance into consideration.
 
     if byte <= 0:
-        return '0'
+        return ('0', None)
     if byte < 2**10:
-        return '{0:d}{1:s}B'.format(byte, separator)
+        return ('{0:d}'.format(byte), 0)
     if byte < 2**10 * 999:
-        return '{0:.3g}{1:s}K'.format((byte / 2**10), separator)
+        return ('{0:.3g}'.format(byte / 2**10), 1)
     if byte < 2**20:
-        return '{0:.4g}{1:s}K'.format((byte / 2**10), separator)
+        return ('{0:.4g}'.format(byte / 2**10), 1)
     if byte < 2**20 * 999:
-        return '{0:.3g}{1:s}M'.format((byte / 2**20), separator)
+        return ('{0:.3g}'.format(byte / 2**20), 2)
     if byte < 2**30:
-        return '{0:.4g}{1:s}M'.format((byte / 2**20), separator)
+        return ('{0:.4g}'.format(byte / 2**20), 2)
     if byte < 2**30 * 999:
-        return '{0:.3g}{1:s}G'.format((byte / 2**30), separator)
+        return ('{0:.3g}'.format(byte / 2**30), 3)
     if byte < 2**40:
-        return '{0:.4g}{1:s}G'.format((byte / 2**30), separator)
+        return ('{0:.4g}'.format(byte / 2**30), 3)
     if byte < 2**40 * 999:
-        return '{0:.3g}{1:s}T'.format((byte / 2**40), separator)
+        return ('{0:.3g}'.format(byte / 2**40), 4)
     if byte < 2**50:
-        return '{0:.4g}{1:s}T'.format((byte / 2**40), separator)
+        return ('{0:.4g}'.format(byte / 2**40), 4)
     if byte < 2**50 * 999:
-        return '{0:.3g}{1:s}P'.format((byte / 2**50), separator)
+        return ('{0:.3g}'.format(byte / 2**50), 5)
     if byte < 2**60:
-        return '{0:.4g}{1:s}P'.format((byte / 2**50), separator)
-    return '>9000'
+        return ('{0:.4g}'.format(byte / 2**50), 5)
+    return ('>9000', None)
 
 
 def human_readable_time(timestamp):
