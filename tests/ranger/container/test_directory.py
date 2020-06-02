86a86dd40b05e1b53323abeea57c7edf72d31d8c
German Umarov <g_umarov@gmail.com>
2020-06-02 19:33:23 +0300

    fix unit-tests

--- tests/ranger/container/test_directory.py
+++ tests/ranger/container/test_directory.py
@@ -82,17 +82,26 @@ def create_tree_with_symlinks_no_loop(tmp_path):
     symlink1.symlink_to(file1)
 
 
+def copy_level(level):
+    level_copy = copy.deepcopy(list(level))
+    level_copy[1] = sorted(level_copy[1])  # because os.walk yields dirnames in arbitraty order
+    return tuple(level_copy)
+
+
 def test_walklevel_all_levels_no_symlinks(tmp_path):
     create_tree_no_symlinks(tmp_path)
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), -1):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
-    assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None),
-                                 (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
-                                 (str(tmp_path / 'a' / 'c'), [], ['file2.txt'], None),
-                                 (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+    expected_levels = [(str(tmp_path), ['a', 'b'], [], None),
+                       (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
+                       (str(tmp_path / 'a' / 'c'), [], ['file2.txt'], None),
+                       (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+
+    for resulting_level in walklevel_results:
+        assert resulting_level in expected_levels
 
 
 def test_walklevel_first_level_no_symlinks(tmp_path):
@@ -100,11 +109,14 @@ def test_walklevel_first_level_no_symlinks(tmp_path):
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), 1):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
-    assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None),
-                                 (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
-                                 (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+    expected_levels = [(str(tmp_path), ['a', 'b'], [], None),
+                       (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
+                       (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+
+    for resulting_level in walklevel_results:
+        assert resulting_level in expected_levels
 
 
 def test_walklevel_all_levels_symlinks_with_loop(tmp_path):
@@ -112,13 +124,16 @@ def test_walklevel_all_levels_symlinks_with_loop(tmp_path):
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), -1, follow_symlinks=True):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
-    assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None),
-                                 (str(tmp_path / 'a'), ['c'], ['file1.txt'],
-                                  str(tmp_path / 'a' / 'link1')),
-                                 (str(tmp_path / 'a' / 'c'), [], ['file2.txt'], None),
-                                 (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+    expected_levels = [(str(tmp_path), ['a', 'b'], [], None),
+                       (str(tmp_path / 'a'), ['c'], ['file1.txt'],
+                        str(tmp_path / 'a' / 'link1')),
+                       (str(tmp_path / 'a' / 'c'), [], ['file2.txt'], None),
+                       (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+
+    for resulting_level in walklevel_results:
+        assert resulting_level in expected_levels
 
 
 def test_walklevel_first_level_symlinks_with_loop(tmp_path):
@@ -126,12 +141,15 @@ def test_walklevel_first_level_symlinks_with_loop(tmp_path):
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), 1, follow_symlinks=True):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
-    assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None),
-                                 (str(tmp_path / 'a'), ['c'], ['file1.txt'],
-                                  str(tmp_path / 'a' / 'link1')),
-                                 (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+    expected_levels = [(str(tmp_path), ['a', 'b'], [], None),
+                       (str(tmp_path / 'a'), ['c'], ['file1.txt'],
+                        str(tmp_path / 'a' / 'link1')),
+                       (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+
+    for resulting_level in walklevel_results:
+        assert resulting_level in expected_levels
 
 
 def test_walklevel_all_levels_symlinks_without_loop(tmp_path):
@@ -139,12 +157,16 @@ def test_walklevel_all_levels_symlinks_without_loop(tmp_path):
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), -1, follow_symlinks=True):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
-    assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None),
-                                 (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
-                                 (str(tmp_path / 'a' / 'c'), [], ['file2.txt', 'link1'], None),
-                                 (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+    expected_levels = [(str(tmp_path), ['a', 'b'], [], None),
+                       (str(tmp_path / 'a'), ['c'], ['file1.txt'], None),
+                       (str(tmp_path / 'a' / 'c'), [],
+                        ['file2.txt', 'link1'], None),
+                       (str(tmp_path / 'b'), [], ['file3.txt'], None)]
+
+    for resulting_level in walklevel_results:
+        assert resulting_level in expected_levels
 
 
 def test_walklevel_zero_level_no_symlinks(tmp_path):
@@ -152,6 +174,6 @@ def test_walklevel_zero_level_no_symlinks(tmp_path):
 
     walklevel_results = []
     for level in walklevel(str(tmp_path), 0):
-        walklevel_results.append(copy.deepcopy(level))
+        walklevel_results.append(copy_level(level))
 
     assert walklevel_results == [(str(tmp_path), ['a', 'b'], [], None)]
