51bf9caa725c048c75fae114ac7e82ed36148d56
chu4ng <shichuang@horsefucker.org>
2020-12-02 07:38:23 +0300

    Adding documentation for sxiv_open_with and some cosmetic changes.

--- examples/plugin_sxiv.py
+++ examples/plugin_sxiv.py
@@ -2,26 +2,23 @@
 
 from __future__ import (absolute_import, division, print_function)
 
+import subprocess
 
 from ranger.api.commands import Command
-import ranger.api
 from ranger.config.commands import scout
 from ranger.ext.rifle import Rifle
 
-import subprocess
 
 old_execute = Rifle.execute
 
 class sxiv_select(Command):
-    """
-    :sxiv_select
-    
-    Opens sxiv in thimb mode and marked files from him will be selected in ranger.
+    """:sxiv_select
+
+    Opens sxiv in thumb mode and marked files from him will be selected in ranger.
     If one file was mark, ranger move cursot to this file.
     If you have a few files selected in ranger and run this command, sxiv opens with this files.
     """
     def execute(self):
-        null_sep = {'arg': '-0', 'split': '\0'}
         nl_sep = {'arg': '', 'split': '\n'}
         result_sep = nl_sep
 
@@ -32,8 +29,10 @@ class sxiv_select(Command):
             images = [f for f in self.fm.thisdir.files if f.image]
         # Create subprocess for sxiv and pipeout in whatever
         sxiv_args = ['-o', '-t', '--'] + [i.relative_path for i in images]
-        process = subprocess.Popen(['sxiv'] + sxiv_args, universal_newlines=True, stdout=subprocess.PIPE)
-        (pipe_out, _err) = process.communicate()
+        process = subprocess.Popen(['sxiv'] + sxiv_args,
+                universal_newlines=True,
+                stdout=subprocess.PIPE)
+        (pipe_out, _) = process.communicate()
         raw_out = pipe_out.split(result_sep['split'])
         # Delete empty
         marked_files = list(filter(None, raw_out))
@@ -44,12 +43,17 @@ class sxiv_select(Command):
                     node_f.mark_set(True)
             self.fm.ui.redraw_window()
         elif len(marked_files) == 1:
-            sc = scout("-ts " + marked_files[0])
-            sc.execute()
+            scout_obj = scout("-ts " + marked_files[0])
+            scout_obj.execute()
 
 def sxiv_open_with(self, files, number=0, label=None, flags="", mimetype=None):
-    command = None
-    for count, cmd, lbl, flgs in self.list_commands(files, mimetype):
+    """
+    Hooking for sxiv_select option from rifle menu,
+    which handle call of not exist programm and run command sxiv_select.
+    """
+
+    cmd = ""
+    for count, cmd, lbl, _flags in self.list_commands(files, mimetype):
         if label and label == lbl or not label and count == number:
             cmd = self.hook_command_preprocessing(cmd)
             break
@@ -59,8 +63,8 @@ def sxiv_open_with(self, files, number=0, label=None, flags="", mimetype=None):
             cmd = '%s "$@"' % label
 
     if cmd == "sxiv_select":
-        s = sxiv_select("")
-        s.execute()
+        sxiv_obj = sxiv_select("")
+        sxiv_obj.execute()
     else:
         global old_execute
         old_execute(self, files, number, label, flags, mimetype)
