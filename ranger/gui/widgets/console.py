b3279652597dcf9c3b2db8699f4a311d24730267
toonn <toonn@toonn.io>
2021-01-16 22:20:11 +0100

    transpose_subr/words: Format in accordance to PEP8

--- ranger/gui/widgets/console.py
+++ ranger/gui/widgets/console.py
@@ -479,8 +479,7 @@ class Console(Widget):  # pylint: disable=too-many-instance-attributes,too-many-
             # If in/after last word, interchange last two words
             if (re.match(r'[\w\d]*\s*$', self.line[self.pos:], re.UNICODE)
                 and (re.match(r'[\w\d]', self.line[self.pos - 1], re.UNICODE)
-                     if self.pos -1 >= 0 else True)
-               ):
+                     if self.pos - 1 >= 0 else True)):
                 self.pos = self.move_by_word(self.line, self.pos, -1)
 
             # Util function to increment position until out of word/whitespace
