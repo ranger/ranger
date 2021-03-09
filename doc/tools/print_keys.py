36a2ede4e8b545ee718dcfe77207e2c696d73125
XuehaiPan <XuehaiPan@pku.edu.cn>
2021-03-09 22:09:42 +0800

    Refactor keybinding_parser.py

--- doc/tools/print_keys.py
+++ doc/tools/print_keys.py
@@ -6,18 +6,29 @@ You can use this tool to find out values of keypresses
 from __future__ import (absolute_import, division, print_function)
 
 import curses
+import os
+import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+from ranger.ext.keybinding_parser import special_keys, reversed_special_keys, construct_keybinding
 
 SEPARATOR = '; '
 
 
 @curses.wrapper
-def main(window):
+def main(win):
     curses.mousemask(curses.ALL_MOUSE_EVENTS)
     curses.mouseinterval(0)
     while True:
-        char = window.getch()
+        char = win.getch()
+        string = ''
         if char == curses.KEY_MOUSE:
-            window.addstr(repr(curses.getmouse()) + SEPARATOR)
+            string = repr(curses.getmouse()) + SEPARATOR
         else:
-            window.addstr(str(char) + SEPARATOR)
+            string = str(char) + '(%s)' % construct_keybinding(char) + SEPARATOR
+        try:
+            win.addstr(string)
+        except curses.error:
+            win.erase()
+            win.addstr(string)
