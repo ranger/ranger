6db53d9a8a6c9d30b63330bd9edb2f749c8c02f7
toonn <toonn@toonn.io>
2021-07-17 22:50:56 +0200

    config.commands: Refactor to use with
    This required adding another argument to `open23` so I just implemented
    all the arguments the open builtin has.

--- ranger/ext/open23.py
+++ ranger/ext/open23.py
@@ -7,18 +7,41 @@ from contextlib import contextmanager
 
 from ranger import PY3
 
+
 # COMPAT: We use the pattern of opening a file differently depending on the
 #         python version in multiple places. Having calls to open in multiple
 #         branches makes it impossible to use a with-statement instead. This
 #         contextmanager hides away the lack of an errors keyword argument for
 #         python 2 and is now preferred. This can be safely dropped once we
 #         ditch python 2 support.
+# pylint: disable=too-many-arguments
 @contextmanager
-def open23(file, mode="r", errors=None):
+def open23(
+    file,
+    mode="r",
+    buffering=-1,
+    encoding=None,
+    errors=None,
+    newline=None,
+    closefd=True,
+    opener=None,
+):
     if PY3:
-        fobj = open(file, mode, errors)
+        fobj = open(
+            file=file,
+            mode=mode,
+            buffering=buffering,
+            encoding=encoding,
+            errors=errors,
+            newline=newline,
+            closefd=closefd,
+            opener=opener,
+        )
     else:
-        fobj = open(file, mode)
+        if buffering is None:
+            fobj = open(name=file, mode=mode)
+        else:
+            fobj = open(name=file, mode=mode, buffering=buffering)
     try:
         yield fobj
     finally:
