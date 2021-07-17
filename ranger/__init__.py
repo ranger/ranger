33dc0952e7bc366b70eb989dc7d540760009e1a7
toonn <toonn@toonn.io>
2021-07-17 21:39:14 +0200

    ranger.__init__: Refactor to use with

--- ranger/__init__.py
+++ ranger/__init__.py
@@ -22,12 +22,14 @@ def version_helper():
         import subprocess
         version_string = 'ranger-master {0}'
         try:
-            git_describe = subprocess.Popen(['git', 'describe'],
-                                            universal_newlines=True,
-                                            cwd=RANGERDIR,
-                                            stdout=subprocess.PIPE,
-                                            stderr=subprocess.PIPE)
-            (git_description, _) = git_describe.communicate()
+            with subprocess.Popen(
+                ["git", "describe"],
+                universal_newlines=True,
+                cwd=RANGERDIR,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            ) as git_describe:
+                (git_description, _) = git_describe.communicate()
             version_string = version_string.format(git_description.strip('\n'))
         except (OSError, subprocess.CalledProcessError):
             version_string = version_string.format(__version__)
