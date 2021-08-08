056f6895d403a98c78047f5ded48984864ed02aa
toonn <toonn@toonn.io>
2021-08-08 19:43:49 +0200

    spawn: Use Popen23 for compatibility

--- ranger/ext/spawn.py
+++ ranger/ext/spawn.py
@@ -4,7 +4,10 @@
 from __future__ import (absolute_import, division, print_function)
 
 from os import devnull
-from subprocess import Popen, PIPE, CalledProcessError
+from subprocess import PIPE, CalledProcessError
+
+from ranger.ext.popen23 import Popen23
+
 ENCODING = 'utf-8'
 
 
@@ -28,11 +31,11 @@ def check_output(popenargs, **kwargs):
     kwargs.setdefault('shell', isinstance(popenargs, str))
 
     if 'stderr' in kwargs:
-        with Popen(popenargs, **kwargs) as process:
+        with Popen23(popenargs, **kwargs) as process:
             stdout, _ = process.communicate()
     else:
         with open(devnull, mode='w') as fd_devnull:
-            with Popen(popenargs, stderr=fd_devnull, **kwargs) as process:
+            with Popen23(popenargs, stderr=fd_devnull, **kwargs) as process:
                 stdout, _ = process.communicate()
 
     if process.returncode != 0:
