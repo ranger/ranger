74d410a9f8bd3b00ce4f1429b048245c50f34ff0
chu4ng <shichuang@horsefucker.org>
2021-08-08 14:54:22 +0300

    The adding OCS 52 support for copying filename-related data via terminal features.

--- ranger/config/commands.py
+++ ranger/config/commands.py
@@ -1981,11 +1981,22 @@ class yank(Command):
         selection = self.get_selection_attr(mode)
 
         new_clipboard_contents = "\n".join(selection)
-        for command in clipboard_commands:
-            with subprocess.Popen(
-                command, universal_newlines=True, stdin=subprocess.PIPE
-            ) as process:
-                process.communicate(input=new_clipboard_contents)
+        if len(clipboard_commands) < 1:
+
+            import base64
+
+            tty_num = subprocess.check_output(["tty"]).strip()
+            with open(tty_num, 'wb') as stdout:
+                base64Content = b'\033]52;c;' + \
+                        base64.b64encode(new_clipboard_contents.encode('utf-8')) + \
+                        b'\a'
+                stdout.write(base64Content)
+        else:
+            for command in clipboard_commands:
+                with subprocess.Popen(
+                        command, universal_newlines=True, stdin=subprocess.PIPE
+                        ) as process:
+                    process.communicate(input=new_clipboard_contents)
 
     def get_selection_attr(self, attr):
         return [getattr(item, attr) for item in
