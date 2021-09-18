7796d2ea4530dce798f5d3f747da3d50f5c13b65
Leo Vivier <zaeph@zaeph.net>
2021-09-18 14:04:05 +0200

    Add check
    I'm still getting `'NoneType' object has no attribute 'request_redraw'`
    errors, so I'm probably fucking up the loading of request_redraw; it still
    works, though.

--- ranger/container/settings.py
+++ ranger/container/settings.py
@@ -192,7 +192,8 @@ class Settings(SignalDispatcher, FileManagerAware):
                    path=path, tags=tags, fm=self.fm)
         self.signal_emit('setopt', **kws)
         self.signal_emit('setopt.' + name, **kws)
-        self.fm.ui.status.request_redraw()
+        if name == 'show_hidden':
+            self.fm.ui.status.request_redraw()
 
     def _get_default(self, name):
         if name == 'preview_script':
