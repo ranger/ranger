e7f2788571762a2bcda3aeaba51a9e843d887c64
toonn <toonn@toonn.io>
2022-02-05 19:29:51 +0100

    Revert "loader: Drop disappeared Pylint options"
    This reverts commit c3824a59b28fa4895e44718edebc144859eac233.

--- ranger/core/loader.py
+++ ranger/core/loader.py
@@ -175,6 +175,7 @@ class CommandLoader(  # pylint: disable=too-many-instance-attributes
         # pylint: disable=too-many-branches,too-many-statements
         # TODO: Check whether we can afford to wait for processes and use a
         #       with-statement for Popen.
+        # pylint: disable=consider-using-with
         popenargs = {} if self.popenArgs is None else self.popenArgs
         popenargs['stdout'] = popenargs['stderr'] = PIPE
         popenargs['stdin'] = (
