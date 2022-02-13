49bb550cb2fbef22e2b10782d6aa2afe0dc61943
Nici <NiciTheNici@protonmail.com>
2022-02-13 19:14:34 +0100

    fix indent

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -49,7 +49,7 @@ OPENSCAD_COLORSCHEME="${RNGR_OPENSCAD_COLORSCHEME:-Tomorrow Night}"
 
 handle_extension() {
     case "${FILE_EXTENSION_LOWER}" in
-	## Archive
+        ## Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
             atool --list -- "${FILE_PATH}" && exit 5
