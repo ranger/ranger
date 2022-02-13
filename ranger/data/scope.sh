93ace2a4b6e9788adecd690020dc57e576c608cd
Nici <NiciTheNici@protonmail.com>
2022-02-13 18:50:30 +0100

    add drawio Image-Preview

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -49,7 +49,7 @@ OPENSCAD_COLORSCHEME="${RNGR_OPENSCAD_COLORSCHEME:-Tomorrow Night}"
 
 handle_extension() {
     case "${FILE_EXTENSION_LOWER}" in
-        ## Archive
+	## Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
             atool --list -- "${FILE_PATH}" && exit 5
@@ -90,6 +90,11 @@ handle_extension() {
             ## Preview as text conversion (unsupported by pandoc for markdown)
             odt2txt "${FILE_PATH}" && exit 5
             exit 1;;
+		
+#       drawio)
+#           ## Preview as image
+#           draw.io -x "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" --width 1920 && exit 6
+#           exit 1;;
 
         ## XLSX
         xlsx)
