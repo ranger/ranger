dfebd3d2f7fa0b66d70b53367a9e40c970809d86
qrvd <36740654+qrvd@users.noreply.github.com>
2022-10-24 04:10:02 +0200

    fm: port the signals fix to older Python versions

--- ranger/core/fm.py
+++ ranger/core/fm.py
@@ -9,6 +9,7 @@ from time import time
 from collections import deque
 import mimetypes
 import os.path
+import os
 import pwd
 import socket
 import stat
@@ -33,16 +34,17 @@ from ranger.ext import logutils
 
 
 def _call_signal_handler(handler, signum, frame):
-    if handler in signal.Handlers:
-        # the handler is one of signal.Handlers.SIG_DFL or signal.Handlers.SIG_IGN.
-        # these are not callable, so we need to reset the signal and raise them manually.
+    try:
+        handler(signum, frame)
+    except TypeError:
+        # the handler is not callable, so we need to reset the signal and raise it manually.
         prev_handler = signal.signal(signum, handler)
         try:
-            signal.raise_signal(signum)
+            # COMPAT: signal.raise_signal is unavailable in Python <3.8,
+            # but os.kill accomplishes the same thing.
+            os.kill(os.getpid(), signum)
         finally:
             signal.signal(signum, prev_handler)
-    else:
-        handler(signum, frame)
 
 
 def _insert_signal_hook(signum, hook):
