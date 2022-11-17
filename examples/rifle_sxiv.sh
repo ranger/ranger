98ae07b62f9e68c4658c34d432ff590db3e54f13
toonn <toonn@toonn.io>
2022-11-17 12:49:25 +0100

    rifle_sxiv: Update comments

--- examples/rifle_sxiv.sh
+++ examples/rifle_sxiv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Compatible with ranger 1.6.0 through 1.7.*
+# Compatible with ranger 1.6.0 through 1.9.*
 #
 # This script searches image files in a directory, opens them all with sxiv and
 # sets the first argument to the first image displayed by sxiv.
@@ -12,20 +12,20 @@
 #
 # Implementation note: the script tries to be POSIX compliant both in terms of
 # shell syntax and calls being made to external utilities, such as grep or find.
-# This makes it portable across many unix like systems, although it may not be
+# This makes it portable across many unix-like systems, although it may not be
 # the cleanest or fastest approach.
 #
-# First, using case statement to get absolute path is quicker than calling
-# 'realpath' because it would fork a whole process, which is slow.
+# First, using a case statement to get the absolute path is quicker than
+# calling 'realpath' because it would fork an entire process, which is slow.
 #
 # Second, we need to append a file list to sxiv, which can only be done
-# properly in three ways: arrays (which are not POSIX).
-# \0 separated strings; but assigning \0 to a variable is not POSIX either
-# so we cannot store the result of listfiles to a variable.
-#
-# The third approach is to store the result to a tmpfile and using `-i` to feed
-# the list to sxiv. This is the fastest approach since we won't have to call
-# listfiles twice.
+# properly in three ways:
+# - arrays (which are not POSIX).
+# - \0 separated strings; but assigning \0 to a variable is not POSIX either
+#   so we cannot store the result of listfiles in a variable.
+# - The third approach is to store the result to a tempfile and use `-i` to
+#   feed the list to sxiv. This is the fastest approach since we won't have to
+#   call listfiles twice.
 
 
 TMPDIR="${TMPDIR:-/tmp}"
@@ -41,7 +41,7 @@ listfiles () {
 }
 
 open_img () {
-    # only go through listfiles() if the file has a valid img extension
+    # Only go through listfiles() if the file has a valid img extension
     if echo "$1" | is_img_extension >/dev/null 2>&1; then
         trap 'rm -f $tmp' EXIT
         count="$(listfiles "$1" | grep -nF "$1")"
@@ -49,8 +49,8 @@ open_img () {
     if [ -n "$count" ]; then
         sxiv -i -n "${count%%:*}" -- < "$tmp"
     else
-        # fallback incase file didn't have a valid extension, or we couldn't
-        # find it inside the list
+        # Fallback in case the file didn't have a valid extension, or we
+        # couldn't find it inside the list
         sxiv -- "$@"
     fi
 }
