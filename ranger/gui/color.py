82eef55b1664a66c774df969673435eb7bc82091
toonn <toonn@toonn.io>
2023-03-07 17:27:59 +0100

    color: Catch Color number is greater than COLORS-1
    Some terminals do not support the number of color pairs we use. Python's
    curses module used to throw a curses.error for this case but in Python
    3.10 this was changed to ValueError.
    
    Approximating the pair with the closest one that is already set up might
    be a viable fix but very few terminals are so limited in their color
    support and it might make things subtly off but hard to put your finger
    on. So we are falling back to the default white on black pair.
    
    Fixes #2498
    Closes #2741
    Fixes #2752
    Fixes #2761
    Fixes #2800

--- ranger/gui/color.py
+++ ranger/gui/color.py
@@ -19,6 +19,7 @@ import curses
 
 DEFAULT_FOREGROUND = curses.COLOR_WHITE
 DEFAULT_BACKGROUND = curses.COLOR_BLACK
+# Color pair 0 is wired to white on black and cannot be changed
 COLOR_PAIRS = {(DEFAULT_FOREGROUND, DEFAULT_BACKGROUND): 0}
 
 
@@ -30,6 +31,12 @@ def get_color(fg, bg):
         size = len(COLOR_PAIRS)
         try:
             curses.init_pair(size, fg, bg)
+        except ValueError:
+            # We're trying to add more pairs than the terminal can store,
+            # approximating to the closest color pair that's already stored
+            # would be cool but the easier solution is to just fall back to the
+            # default fore and background colors, pair 0
+            COLOR_PAIRS[key] = 0
         except curses.error:
             # If curses.use_default_colors() failed during the initialization
             # of curses, then using -1 as fg or bg will fail as well, which
