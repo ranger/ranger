b604e8bd85379ea3ec424deb8596cdec78f6ec99
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-03-31 12:58:19 +0000

    Reformat script

--- ranger/data/scope.sh
+++ ranger/data/scope.sh
@@ -329,25 +329,24 @@ handle_mime() {
         *sqlite3)
             ## Preview as text conversion
             sqlite_tables="$( sqlite3 "file:${FILE_PATH}?mode=ro" '.tables' )" \
-              || exit 1
-            [ -z "${sqlite_tables}" ] && \
-              { echo "Empty SQLite database." && exit 5; }
+                || exit 1
+            [ -z "${sqlite_tables}" ] &&
+                { echo "Empty SQLite database." && exit 5; }
             sqlite_show_query() {
                 sqlite-utils query "${FILE_PATH}" "${1}" --table --fmt fancy_grid \
-                  || sqlite3 "file:${FILE_PATH}?mode=ro" "${1}" -header -column
+                || sqlite3 "file:${FILE_PATH}?mode=ro" "${1}" -header -column
             }
             ## Display basic table information
             sqlite_rowcount_query="$(
                 sqlite3 "file:${FILE_PATH}?mode=ro" -noheader \
-                    'SELECT (
-                        group_concat("SELECT
-                          """ || name || """ AS tblname,
-                          count(*) AS rowcount
-                          FROM " || name
-                        , " UNION ALL "
-                        )
-                    ) FROM sqlite_master
-                      WHERE type="table" AND name NOT LIKE "sqlite_%";'
+                    'SELECT group_concat(
+                        "SELECT """ || name || """ AS tblname,
+                                          count(*) AS rowcount
+                         FROM " || name,
+                        " UNION ALL "
+                    )
+                    FROM sqlite_master
+                    WHERE type="table" AND name NOT LIKE "sqlite_%";'
             )"
             sqlite_show_query \
                 'SELECT tblname AS "table", rowcount AS "count",
@@ -357,55 +356,56 @@ handle_mime() {
                 ) AS "columns",
                 (
                     SELECT "(" || group_concat(
-                      upper(type) || (CASE WHEN pk > 0
-                        THEN " PRIMARY KEY" ELSE "" END)
-                      , ", "
-                      ) || ")"
+                        upper(type) || (
+                            CASE WHEN pk > 0 THEN " PRIMARY KEY" ELSE "" END
+                        ),
+                        ", "
+                    ) || ")"
                     FROM pragma_table_info(tblname)
                 ) AS "types"
                 FROM '"(${sqlite_rowcount_query});"
-            if [ "${SQLITE_TABLE_LIMIT}" -gt 0 ] \
-               && [ "${SQLITE_ROW_LIMIT}" -ge 0 ]; then
+            if [ "${SQLITE_TABLE_LIMIT}" -gt 0 ] &&
+               [ "${SQLITE_ROW_LIMIT}" -ge 0 ]; then
                 ## Do exhaustive preview
-                echo
-                printf '>%.0s' $( seq "${PV_WIDTH}" )
-                echo
+                echo && printf '>%.0s' $( seq "${PV_WIDTH}" ) && echo
                 sqlite3 "file:${FILE_PATH}?mode=ro" -noheader \
-                    "SELECT name FROM sqlite_master
-                      WHERE type='table' AND name NOT LIKE 'sqlite_%'
-                      LIMIT ${SQLITE_TABLE_LIMIT};" |
+                    'SELECT name FROM sqlite_master
+                    WHERE type="table" AND name NOT LIKE "sqlite_%"
+                    LIMIT '"${SQLITE_TABLE_LIMIT}"';' |
                     while read -r sqlite_table; do
-                        sqlite_table_rowcount="$(
-                          sqlite3 "file:${FILE_PATH}?mode=ro" -noheader \
-                            "SELECT count(*) FROM ${sqlite_table}"
+                        sqlite_rowcount="$(
+                            sqlite3 "file:${FILE_PATH}?mode=ro" -noheader \
+                                "SELECT count(*) FROM ${sqlite_table}"
                         )"
-                        if [ "${SQLITE_ROW_LIMIT}" -gt 0 ] \
-                           && [ "${SQLITE_ROW_LIMIT}" \
-                              -lt "${sqlite_table_rowcount}" ]; then
-                            echo
-                            echo "${sqlite_table} [${SQLITE_ROW_LIMIT} of ${sqlite_table_rowcount}]:"
+                        echo
+                        if [ "${SQLITE_ROW_LIMIT}" -gt 0 ] &&
+                           [ "${SQLITE_ROW_LIMIT}" \
+                             -lt "${sqlite_rowcount}" ]; then
+                            echo "${sqlite_table} [${SQLITE_ROW_LIMIT} of ${sqlite_rowcount}]:"
                             sqlite_ellipsis_query="$(
                                 sqlite3 "file:${FILE_PATH}?mode=ro" -noheader \
-                                  "SELECT 'SELECT ' || group_concat(
-                                    '\"...\"'
-                                    , ' , '
+                                    'SELECT "SELECT " || group_concat(
+                                        """...""", ", "
                                     )
-                                    FROM pragma_table_info('${sqlite_table}');"
+                                    FROM pragma_table_info(
+                                        "'"${sqlite_table}"'"
+                                    );'
                             )"
                             sqlite_show_query \
-                              'SELECT *
-                               FROM (SELECT * FROM '"${sqlite_table}"' LIMIT 1)
-                               UNION ALL '"${sqlite_ellipsis_query}"'
-                               UNION ALL SELECT * FROM (
-                                 SELECT * FROM '"${sqlite_table}"'
-                                   LIMIT ('"${SQLITE_ROW_LIMIT}"' - 1)
-                                   OFFSET ('"${sqlite_table_rowcount}"'
-                                          - ('"${SQLITE_ROW_LIMIT}"' - 1)
-                                          )
-                               );'
+                                'SELECT * FROM (
+                                    SELECT * FROM '"${sqlite_table}"' LIMIT 1
+                                )
+                                UNION ALL '"${sqlite_ellipsis_query}"' UNION ALL
+                                SELECT * FROM (
+                                    SELECT * FROM '"${sqlite_table}"'
+                                    LIMIT ('"${SQLITE_ROW_LIMIT}"' - 1)
+                                    OFFSET (
+                                        '"${sqlite_rowcount}"'
+                                        - ('"${SQLITE_ROW_LIMIT}"' - 1)
+                                    )
+                                );'
                         else
-                            echo
-                            echo "${sqlite_table} [${sqlite_table_rowcount}]:"
+                            echo "${sqlite_table} [${sqlite_rowcount}]:"
                             sqlite_show_query "SELECT * FROM ${sqlite_table};"
                         fi
                     done
