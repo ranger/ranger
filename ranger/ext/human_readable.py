e93b85d00b91fb00e4398d7f52765b934924adde
Szymon Hankus <dephan@protonmail.com>
2023-04-01 19:32:37 +0200

    Added a default argument to the human_readable function

--- ranger/ext/human_readable.py
+++ ranger/ext/human_readable.py
@@ -8,22 +8,22 @@ from datetime import datetime
 from ranger.core.shared import SettingsAware
 
 
-def human_readable(byte_count, separator=' '):  # pylint: disable=too-many-return-statements
+def human_readable(byte_count, separator=' ', use_binary=None):  # pylint: disable=too-many-return-statements
     """Convert a large number of bytes to an easily readable format.
-    Output depends on the binary_size_prefix setting, which is False by default.
+    Output depends on the binary_size_prefix setting (which is False by default)
+    and on the use_binary argument. The latter has priority over the former.
 
-    >>> human_readable(54)
+    >>> human_readable(54, use_binary=False)
     '54 B'
-    >>> human_readable(1500)
+    >>> human_readable(1500, use_binary=False)
     '1.5 k'
-    >>> human_readable(2 ** 20 * 1023)
+    >>> human_readable(2 ** 20 * 1023, use_binary=False)
     '1.07 G'
-    >>> SettingsAware.settings.binary_size_prefix = True
-    >>> human_readable(54)
+    >>> human_readable(54, use_binary=True)
     '54 B'
-    >>> human_readable(1500)
+    >>> human_readable(1500, use_binary=True)
     '1.46 Ki'
-    >>> human_readable(2 ** 20 * 1023)
+    >>> human_readable(2 ** 20 * 1023, use_binary=True)
     '1023 Mi'
     """
 
@@ -36,7 +36,7 @@ def human_readable(byte_count, separator=' '):  # pylint: disable=too-many-retur
         return format(byte_count, 'n')  # 'n' = locale-aware separator.
 
     # If you attempt to shorten this code, take performance into consideration.
-    binary = bool(SettingsAware.settings.binary_size_prefix)
+    binary = use_binary if use_binary is not None else bool(SettingsAware.settings.binary_size_prefix)
     if binary:
         prefixes = ('B', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi')
         unit = 1024
