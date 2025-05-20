01ad68a029fe3cd7c421c4ac41519917dafb9f2d
qrvd <36740654+qrvd@users.noreply.github.com>
2025-05-20 02:34:00 +0200

    runner/fm: switch to process count instead of UUID

--- ranger/core/fm.py
+++ ranger/core/fm.py
@@ -175,7 +175,7 @@ class FM(Actions,  # pylint: disable=too-many-instance-attributes
             # that would modify the terminal mode, must be checked here!
             if self.rifle.is_waiting():
                 return False
-            elif self.run is not None and len(self.run.ui_process_tokens) > 0:
+            elif self.run is not None and self.run.ui_process_count > 0:
                 return False
             return True
         _insert_signal_hook(signal.SIGCONT, lambda:
