9946e1e485617d537a55ba5fca09112b15f8c0c3
initumX <initum.x@gmail.com>
2025-06-26 16:34:44 +0300

    Preview archives only if smaller than a specified size ranger#2984

--- ranger/config/commands.py
+++ ranger/config/commands.py
@@ -1011,41 +1011,20 @@ class unmark_tag(mark_tag):
 
 
 class mkdir(Command):
-    """:mkdir <dirname1> [<dirname2> ...]
+    """:mkdir <dirname>
 
-    Creates one or more directories.
-    If directory names are relative, they are created in the current directory.
+    Creates a directory with the name <dirname>.
     """
 
     def execute(self):
-        from os.path import join, expanduser, lexists, abspath
+        from os.path import join, expanduser, lexists
         from os import makedirs
 
-        if not self.rest(1):
-            self.fm.notify("Error: No directory names provided", bad=True)
-            return
-
-        # Split arguments and handle each directory
-        for dirname in self.args[1:]:
-            expanded_dir = expanduser(dirname)
-            full_path = (
-                join(self.fm.thisdir.path, expanded_dir)
-                if not os.path.isabs(expanded_dir)
-                else abspath(expanded_dir)
-            )
-
-            try:
-                if lexists(full_path):
-                    self.fm.notify("Directory already exists: {0}".format(full_path), bad=True)
-                    continue
-
-                makedirs(full_path)
-                self.fm.notify("Created directory: {0}".format(full_path))
-            except OSError as e:
-                self.fm.notify(
-                    "Error creating directory {0}: {1}".format(full_path, str(e)),
-                    bad=True
-                )
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
 
     def tab(self, tabnum):
         return self._tab_directory_content()
