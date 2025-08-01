e0122942be11c8b6f0e9914c9d6bb701df807b2c
Marcel Partap <mpartap@gmx.net>
2025-08-01 21:08:51 +0200

    gui.widgets.statusbar: redraw on show_hidden change
    The signal handler for viewmode change
    - doesn't seem to be neccessary (anymore?)
    - would have to be registered on the settings object
    - contained a typo (setop. instead of setopt.)

--- ranger/gui/widgets/statusbar.py
+++ ranger/gui/widgets/statusbar.py
@@ -42,8 +42,8 @@ class StatusBar(Widget):  # pylint: disable=too-many-instance-attributes
         self.column = column
         self.settings.signal_bind('setopt.display_size_in_status_bar',
                                   self.request_redraw, weak=True)
+        self.settings.signal_bind('setopt.show_hidden', self.request_redraw, weak=True)
         self.fm.signal_bind('tab.layoutchange', self.request_redraw, weak=True)
-        self.fm.signal_bind('setop.viewmode', self.request_redraw, weak=True)
 
     def request_redraw(self):
         self.need_redraw = True
