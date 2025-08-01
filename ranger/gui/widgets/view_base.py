4774963ae8bd0759cc45d871ede9aebe3bbb6333
Marcel Partap <mpartap@gmx.net>
2025-08-01 19:34:40 +0200

    gui.widgets: move _draw_border_rectangle() to view_base and share
    sharing is caring : )
    
    old_draw_borders also already set in base class..

--- ranger/gui/widgets/view_base.py
+++ ranger/gui/widgets/view_base.py
@@ -64,6 +64,20 @@ class ViewBase(Widget, DisplayableContainer):  # pylint: disable=too-many-instan
             except curses.error:
                 pass
 
+    def _draw_border_rectangle(self, left_start, right_end):
+        win = self.win
+        # Draw the border lines
+        win.hline(0, left_start, curses.ACS_HLINE, right_end - left_start)
+        win.hline(self.hei - 1, left_start, curses.ACS_HLINE, right_end - left_start)
+        win.vline(1, left_start, curses.ACS_VLINE, self.hei - 2)
+        win.vline(1, right_end, curses.ACS_VLINE, self.hei - 2)
+        # Draw the four corners
+        rounded = self.settings.draw_borders_rounded
+        self.addch(0,            left_start, '╭' if rounded else curses.ACS_ULCORNER)
+        self.addch(self.hei - 1, left_start, '╰' if rounded else curses.ACS_LLCORNER)
+        self.addch(0,            right_end,  '╮' if rounded else curses.ACS_URCORNER)
+        self.addch(self.hei - 1, right_end,  '╯' if rounded else curses.ACS_LRCORNER)
+
     def _draw_bookmarks(self):
         self.columns[-1].clear_image(force=True)
         self.fm.bookmarks.update_if_outdated()
