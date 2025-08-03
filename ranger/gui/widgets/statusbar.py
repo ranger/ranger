a590d45658aa932065ac88538402bf6d07a1b7a9
Marcel Partap <mpartap@gmx.net>
2025-08-03 20:39:33 +0200

    gui.widgets.statusbar: indicate the lack of all that could be shown
    (To test this, press F to freeze and CTRL-R to reset.. or go to a
    directory on which your user does not have +x permission..)

--- ranger/gui/widgets/statusbar.py
+++ ranger/gui/widgets/statusbar.py
@@ -258,7 +258,10 @@ class StatusBar(Widget):  # pylint: disable=too-many-instance-attributes
         if target is None \
                 or not target.accessible \
                 or (target.is_directory and target.files is None):
+            if target is None or not target.loading:
+                right.add('VOID', base, 'message', 'bad')
             if self.settings.freeze_files:
+                right.add(" ", "space")
                 right.add('FROZEN', base, 'frozen')
             return
 
