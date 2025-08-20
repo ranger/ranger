88fb9978865225ce52b79794f35f142c664df180
Marcel Partap <mpartap@gmx.net>
2025-08-20 11:08:22 +0200

    Clean up plugin code mess to a) be about functional b) not fail tests
    FIXME: still does not work..

--- examples/plugin_fzf_open_with.py
+++ examples/plugin_fzf_open_with.py
@@ -1,4 +1,6 @@
-# This plugin replaces the open_with menu with fzf.
+# pylint: disable=no-member, consider-using-with, consider-using-f-string
+""" This plugin replaces the open_with menu with fzf. """
+
 from __future__ import (absolute_import, division, print_function)
 
 import logging
@@ -12,19 +14,28 @@ LOG = logging.getLogger(__name__)
 DRAW_INFO_OLD = ranger.core.actions.Actions.draw_possible_programs
 HOOK_INIT_OLD = ranger.api.hook_init
 
-# Define a new info drawing function
+
 @classmethod
 def draw_possible_programs_override(self):
-    import pudb
-    pu.db
+    """
+    Wrapper calling the original function, unsetting the draw_info array
+    and invoking fzf instead.
+    """
+
+    # import pudb
+    # pu.db
 
     DRAW_INFO_OLD(self)
     program_info = self.ui.browser.draw_info
+    if len(program_info) < 1:
+        return
+
+    target = self.thistab.get_selection()[0]
     self.ui.browser.draw_info = []
 
     win_hei = self.ui.browser.hei + 2
     win_wid = self.ui.browser.wid
-    fzf_menu_hei = min(win_hei, len(programs) + 3)
+    fzf_menu_hei = min(win_hei, len(program_info) + 3)
 
     self.ui.win.move(win_hei - fzf_menu_hei, 0)
     self.ui.win.refresh()
@@ -34,30 +45,56 @@ def draw_possible_programs_override(self):
     path_str = target.relative_path
     max_len = win_wid - (len(label) + label_pos)
     if len(path_str) > max_len:
-        path_str = path_str[:max_len - 1] + self.ui.browser.ellipsis[self.settings.unicode_ellipsis]
+        path_str = path_str[:max_len - 1] \
+            + self.ui.browser.ellipsis[self.settings.unicode_ellipsis]
 
-    process = subprocess.Popen(['fzf', '--exact', '--accept-nth=1', '--height=%d' % fzf_menu_hei,
-                                '--reverse', '--info=hidden', '--no-sort', '--no-separator',
-                                '--bind', 'home:first', '--bind', 'end:last',
-                                '--border', '--border-label-pos=%d' % label_pos,
-                                '--border-label', label % path_str],
-                               text=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    stdout, stderr = process.communicate(input='\n'.join(program_info))
+    process = subprocess.Popen(
+        [
+            'fzf', '--exact', '--accept-nth=1', '--height=%d' % fzf_menu_hei,
+            '--reverse', '--info=hidden', '--no-sort', '--no-separator',
+            '--bind', 'home:first', '--bind', 'end:last',
+            '--border', '--border-label-pos=%d' % label_pos,
+            '--border-label', label % path_str
+        ], text=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout, _ = process.communicate(input='\n'.join(program_info))
 
     # reset terminal cursor & mouse settings and redraw window
     self.ui.suspend()
     self.ui.initialize()
 
+    if stdout:
+        self.fm.execute_console('open_with %s' % stdout.strip())
+
+
+def _log_function_address():
+    LOG.info("%s hook_init ranger.core.actions.Actions.draw_possible_programs" % __name__
+             + " = %s" % hex(id(ranger.core.actions.Actions.draw_possible_programs))
+             + " draw_possible_programs_override = %s" % hex(id(draw_possible_programs_override)))
+
+
 def hook_init(fm):
-    LOG.info(f"{__name__} hook_init ranger.core.actions.Actions.draw_possible_programs = {hex(id(ranger.core.actions.Actions.draw_possible_programs))} draw_possible_programs_override = {hex(id(draw_possible_programs_override))}")
+    """
+    Hook into ranger init process.
+    """
+
+    # Remove subsequent open_with call from binding
+    fm.execute_console('map r draw_possible_programs')
+
+    _log_function_address()
+
     # FIXME: both ways don't make a difference in behaviour
     # ranger.core.actions.Actions.draw_possible_programs = draw_possible_programs_override
-    ranger.core.actions.Actions.draw_possible_programs = draw_possible_programs_override.__get__(None, ranger.core.actions.Actions)
-    LOG.info(f"{__name__} hook_init ranger.core.actions.Actions.draw_possible_programs = {hex(id(ranger.core.actions.Actions.draw_possible_programs))} draw_possible_programs_override = {hex(id(draw_possible_programs_override))}")
-    
+    ranger.core.actions.Actions.draw_possible_programs \
+        = draw_possible_programs_override.__get__(None, ranger.core.actions.Actions)
+
+    _log_function_address()
+
     return HOOK_INIT_OLD(fm)
+
 
 ranger.api.hook_init = hook_init
 
-# FIXME: also didn't work
-# ranger.core.actions.Actions.draw_possible_programs = draw_possible_programs_override
+# FIXME: this also doesn't work 🤔
+_log_function_address()
+ranger.core.actions.Actions.draw_possible_programs = draw_possible_programs_override
+_log_function_address()
