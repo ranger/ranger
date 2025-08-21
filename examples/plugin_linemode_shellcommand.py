fca6813553ee6c163f808760914c4a259a3ea75e
Marcel Partap <mpartap@gmx.net>
2025-08-21 23:23:12 +0200

    plugin_linemode_shellcommand: lint fixes

--- examples/plugin_linemode_shellcommand.py
+++ examples/plugin_linemode_shellcommand.py
@@ -1,39 +1,51 @@
-# Plugin to add a shellcommand linemode to ranger.
-# ⚠️ DANGER: no sanity checks done, be extra careful!
+"""
+Plugin to add a shellcommand linemode to ranger.
+⚠️ DANGER: no sanity checks done, be extra careful!
+"""
 
 from __future__ import (absolute_import, division, print_function)
 from subprocess import CalledProcessError
 
-import re
 import ranger.api
 
-from ranger.container.settings import ALLOWED_SETTINGS, SIGNAL_PRIORITY_AFTER_SYNC, SIGNAL_PRIORITY_SYNC
+from ranger.container.settings import (
+    ALLOWED_SETTINGS,
+    SIGNAL_PRIORITY_AFTER_SYNC,
+    SIGNAL_PRIORITY_SYNC
+)
 from ranger.core.linemode import LinemodeBase
 from ranger.core.shared import SettingsAware, FileManagerAware
 from ranger.ext.human_readable import human_readable
 from ranger.ext.shell_escape import shell_quote
 from ranger.ext.spawn import check_output
 
-
 HOOK_INIT_OLD = ranger.api.hook_init
-def hook_init(fm):
 
-    # Signal handler for option changes to apply
-    def _reload(self):
+
+def hook_init(fm):
+    """
+    Hook into ranger startup.
+    """
+
+    def _reload():
+        """
+        Signal handler for option changes to apply
+        """
         for tab in fm.tabs.values():
             if tab.thisfile is None:
                 continue
+
             # Purge linemode cache for each matching tab
-            if tab.thisfile.linemode == 'shellcommand':
+            if tab.thisfile.linemode == "shellcommand":
                 for file in tab.thisdir.files:
                     file.display_data = {}
 
     # Setup signal handlers
-    fm.settings.signal_bind('setopt.preview_files', _reload)
+    fm.settings.signal_bind("setopt.preview_files", _reload)
     for suffix in ("", "_format"):
-        name = 'linemode_shellcommand' + suffix
+        name = "linemode_shellcommand" + suffix
         # one to reload on setting changes
-        fm.settings.signal_bind('setopt.' + name, _reload,
+        fm.settings.signal_bind("setopt." + name, _reload,
                                 priority=SIGNAL_PRIORITY_AFTER_SYNC)
 
         # and this handler to make fm.settings actually store these values
@@ -41,7 +53,10 @@ def hook_init(fm):
         # is set up in the settings constructor which is invoked before hook_init()
         # so as it's too late for that, manually hook this one up here
         ALLOWED_SETTINGS[name] = str
-        fm.settings.signal_bind('setopt.' + name, fm.settings._raw_set_with_signal,
+
+        # pylint: disable=protected-access
+        fm.settings.signal_bind("setopt." + name,
+                                fm.settings._raw_set_with_signal,
                                 priority=SIGNAL_PRIORITY_SYNC)
 
     # sensible defaults
@@ -53,9 +68,12 @@ def hook_init(fm):
     fm.execute_console("map MCI default_linemode shellcommand")
 
     # some example commands
-    fm.execute_console("map MCF chain set linemode_shellcommand fortune -s|tr '\t\n' ' '; set linemode_shellcommand_format %%s")
-    fm.execute_console("map MCf set linemode_shellcommand file --brief --mime-type --dereference --special-files %%s")
-    fm.execute_console("map MC5 set linemode_shellcommand echo \"$(timeout 1s md5sum %%s | cut -d' ' -f1)\"|grep -v ^$")
+    fm.execute_console("map MCF chain set linemode_shellcommand fortune -s"
+                       + "|tr '\t\n' ' '; set linemode_shellcommand_format %%s")
+    fm.execute_console("map MCf set linemode_shellcommand file --brief"
+                       + "--mime-type --dereference --special-files %%s")
+    fm.execute_console("map MC5 set linemode_shellcommand echo"
+                       + "\"$(timeout 1s md5sum %%s | cut -d' ' -f1)\"|grep -v ^$")
     fm.execute_console("map MCd set linemode_shellcommand du -hs %%s | tr '\t' ' '")
     fm.execute_console("map MCs set linemode_shellcommand stat -c %%%%F %%s")
 
@@ -64,14 +82,23 @@ def hook_init(fm):
     fm.execute_console("map MC4 set linemode_shellcommand_format %%SIZE%% %%40.40s")
 
     # count PDF pages? Sure! : )
-    fm.execute_console('map MCp chain default_linemode path=pdf$ shellcommand; set linemode_shellcommand_format %%SIZE%%B %%12s; set linemode_shellcommand COUNT=$(pdfinfo %%s|sed -n \'s/^Pages: *//p\') && [ -n "$COUNT" ] && echo "$COUNT page pdf"')
+    fm.execute_console("map MCp chain default_linemode path=pdf$ shellcommand;"
+                       + "set linemode_shellcommand_format %%SIZE%%B %%12s;"
+                       + "set linemode_shellcommand COUNT=$(pdfinfo %%s|sed -n \'s/^Pages: *//p\')"
+                       + '&& [ -n "$COUNT" ] && echo "$COUNT page pdf"')
 
+    fm.notify("foo", bad=True)
     return HOOK_INIT_OLD(fm)
+
 
 ranger.api.hook_init = hook_init
 
+
 @ranger.api.register_linemode
 class ShellCommandLinemode(LinemodeBase, SettingsAware, FileManagerAware):
+    """
+    Provides a shell command linemode.
+    """
     name = "shellcommand"
 
     def filetitle(self, fobj, metadata):
@@ -80,10 +107,10 @@ class ShellCommandLinemode(LinemodeBase, SettingsAware, FileManagerAware):
     def infostring(self, fobj, metadata):
         str_format = self.settings.linemode_shellcommand_format
         cmd = self.settings.linemode_shellcommand
-        cmd = cmd % shell_quote(fobj.path) if '%s' in cmd else cmd
-        sizestring = '' if fobj.size is None or fobj.is_directory else human_readable(fobj.size)
+        cmd = cmd % shell_quote(fobj.path) if "%s" in cmd else cmd
+        sizestring = "" if fobj.size is None or fobj.is_directory else human_readable(fobj.size)
         try:
             output = check_output(["sh", "-c", cmd]).strip()
         except CalledProcessError:
-            return 'PROCESS ERROR'
-        return str_format.replace('%SIZE%', sizestring) % (output)
+            return "PROCESS ERROR"
+        return str_format.replace("%SIZE%", sizestring) % (output)
